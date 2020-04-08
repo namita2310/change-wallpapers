5af9d344b3d534e1d022fee8d54c8e41bfec60bf
namita2310 <namitashetty01@gmail.com>
2020-04-09 00:13:36 +0530

    lil changes done

--- wallpaper.py
+++ wallpaper.py
@@ -4,13 +4,13 @@ import time
 import datetime
 
 def job(t):
-    os.system("`gsettings set org.gnome.desktop.background picture-uri 'file:///home/dell/project/wallpapers/images/team{}.jpg'`".format(t))
+    os.system("`gsettings set org.gnome.desktop.background picture-uri 'file:///home/dell/project/wallpapers/images/{}.jpg'`".format(t))
 
 
 
 x = datetime.datetime.now()
 y=x.strftime("%d")
-schedule.every().day.at("22:08").do(job,y)
+schedule.every().day.at("06:00").do(job,y)
 while True:
     schedule.run_pending()
     time.sleep(10) 
